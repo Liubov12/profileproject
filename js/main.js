1a36aeb81e7fac36079f4abc1555276e6c62c7c4
Liubov Koval <68028382+Liubov12@users.noreply.github.com>
2023-03-13 20:36:18 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const btnDarkMode = document.querySelector(".dark-mode-btn");
+
+btnDarkMode.onclick = function () {
+  btnDarkMode.classList.toggle("dark-mode-btn--active");
+  const isDark = document.body.classList.toggle("dark");
+
+  if (isDark) {
+    localStorage.setItem("darkMode", "dark");
+  } else {
+    localStorage.setItem("darkMode", "light");
+  }
+};
+
+if (localStorage.getItem("darkMode") === "dark") {
+  btnDarkMode.classList.add("dark-mode-btn--active");
+  document.body.classList.add("dark");
+}
+
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  btnDarkMode.classList.add("dark-mode-btn--active");
+  document.body.classList.add("dark");
+}
